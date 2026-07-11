e63d3764799dc063a99cfaa88fc7bbe6812edc23
Herlon R Ludwig <herlonrl@gmail.com>
2026-07-10 21:52:45 -0300

    commit 1

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.get("/team", async (request, response) => {
+    response.type('application/json').code(200)
+
+    return [
+        { id: 1, name: "ferrari" }
+    ]
+});
+
+server.listen({ port: 3333 }, () => {
+    console.log("Server init");
+});
